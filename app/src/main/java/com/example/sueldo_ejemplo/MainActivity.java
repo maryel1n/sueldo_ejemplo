24150c098c2d4722abd74e23ae79efd0fc08bec3
Maryelin Quintanilla <maryelin.quitanilla@walmart.com>
2019-06-19 16:06:47 -0400

    Initial commit

---
+++ app/src/main/java/com/example/sueldo_ejemplo/MainActivity.java
@@ -0,0 +1,72 @@
+package com.example.sueldo_ejemplo;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+import android.widget.CheckBox;
+import android.widget.EditText;
+import android.widget.RadioButton;
+import android.widget.RadioGroup;
+import android.widget.TextView;
+
+public class MainActivity extends AppCompatActivity {
+
+    private EditText txtHoras, txtDias;
+    private CheckBox chbxPago, chbxDcto;
+    private RadioGroup rgRedondeo;
+    private RadioButton rbRedondeo, rbNoRedondeo;
+    private Button btnLimpiar, btnCalcular;
+    private TextView lbl_pago, lbl_dcto;
+
+
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+
+        txtHoras = (EditText)findViewById(R.id.txtHoras);
+        txtDias = (EditText)findViewById(R.id.txtDias);
+        chbxPago = (CheckBox)findViewById(R.id.chbxPago);
+        chbxDcto = (CheckBox)findViewById(R.id.chbxDcto);
+        rgRedondeo = (RadioGroup)findViewById(R.id.rgRedondeo);
+        rbNoRedondeo = (RadioButton)findViewById(R.id.rbNoRedondeo);
+        btnLimpiar = (Button)findViewById(R.id.btnLimpiar);
+        btnCalcular = (Button)findViewById(R.id.btnCalcular);
+        lbl_dcto = (TextView)findViewById(R.id.lbl_dcto);
+        lbl_pago = (TextView)findViewById(R.id.lbl_pago);
+
+    }
+
+    public void calcular (View view){
+
+        int horas = Integer.parseInt(txtHoras.getText().toString());
+        int dias = Integer.parseInt(txtDias.getText().toString());
+        int horas_mensuales = horas*dias;
+        double pago = horas_mensuales*10;
+        double descuento = 0.00;
+        double sueldo_base = 1000.00;
+
+        if(chbxPago.isChecked() == true){
+            lbl_pago.setText(String.valueOf(pago));
+        }
+        if(chbxDcto.isChecked() == true && pago >1000){
+
+            descuento = pago - (pago*0.1);
+            lbl_dcto.setText(String.valueOf(descuento));
+        }
+        if (rgRedondeo.getCheckedRadioButtonId() == R.id.rbRedondeo) {
+            int pago_redondeo = (int)Math.round(pago);
+            lbl_pago.setText(String.valueOf(pago_redondeo));
+            int dcto_redondeo = (int)Math.round((descuento));
+            lbl_dcto.setText(String.valueOf(dcto_redondeo));
+        }
+
+
+    }
+
+    public void limpiar(View view){
+
+    }
+}
